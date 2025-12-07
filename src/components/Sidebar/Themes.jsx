1a39e2e6458526592c6814cb9762815bfdd8bc05
Hanson Emmanuel <hansonemma398@gmail.com>
2025-12-07 13:57:41 +0100

    update to Themes.jsx

--- src/components/Sidebar/Themes.jsx
+++ src/components/Sidebar/Themes.jsx
@@ -1,6 +1,24 @@
+import { Sun, Moon } from 'lucide-react';
+
 const Themes = () => {
     return ( 
-        <div className="p-4">Hi</div>
+        <div className="
+            p-4 flex flex-row rounded-full h-12 bg-red-100
+            justify-around items-center
+        ">
+            <span className='flex items-center'>
+                <Sun className="inline w-5 h-5 mr-2" />
+                <p>Light</p>
+            </span>
+
+            <span className='
+                flex items-center rounded-full bg-red-400 text-white
+                w-1/2 h-full justify-center p-6
+            '>
+                <Moon className="inline w-5 h-5 mr-2" />
+                <p>Dark</p>
+            </span>
+        </div>
      );
 }
  
