42cf49162dc99f951a158943f7cfd58e73f3cddd
Hanson Emmanuel <hansonemma398@gmail.com>
2025-12-07 18:21:35 +0100

    Added some useful info to the about section

--- src/components/Sidebar/About.jsx
+++ src/components/Sidebar/About.jsx
@@ -1,6 +1,11 @@
 const About = () => {
     return ( 
-        <div className="p-4">Hi</div>
+        <div className="p-4">
+            <p>App Name: SmartCalcX</p>
+            <p>Version Number: 1.0.0</p>
+            <p>Email: hansonemma398@gmail.com</p>
+            <p>How to use: <a href="#">link to txt file</a></p>
+        </div>
      );
 }
  
